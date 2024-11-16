4a04bd933c39986e42db664c54b21a619f68087f
TiffanyN22 <tnguyen22@ndsj.org>
2024-11-16 14:44:16 -0800

    Added endpoint to get OnetTitle given occupation keyword

--- flask/jobSalary.py
+++ flask/jobSalary.py
@@ -40,6 +40,31 @@ def jobSalary():
 
   return response.json()["OccupationDetail"]["Wages"]["StateWagesList"][0]
 
+@app.route('/occupation', methods=['GET'])
+def occupation():
+  # get query parameter
+  keyword = request.args.get('keyword')  
+
+  # create request url
+  url = f"https://api.careeronestop.org/v1/occupation/{os.getenv('USER_ID')}/{keyword}/N/0/50"
+
+  # Set headers for the request
+  headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {os.getenv('TOKEN')}" 
+  }
+
+  # Send GET request to the API
+  try:
+      response = requests.get(url, headers=headers)
+      if response.status_code != 200:
+          print(f"Error: {response.status_code}")
+          return response.text 
+  except Exception as e:
+      print(f"An error occurred: {e}")
+
+  return [occupation['OnetTitle'] for occupation in response.json()['OccupationList']]
+
 if __name__ == '__main__':
     app.run(debug=True)
 
