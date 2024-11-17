1e4bcb97563ebbab2a8e93f1283b89086c682092
TiffanyN22 <tnguyen22@ndsj.org>
2024-11-16 22:07:41 -0800

    Added contract verdict and negotiation points

--- ascend/src/Dashboard.js
+++ ascend/src/Dashboard.js
@@ -47,12 +47,54 @@ const AnalyticsGraphs = () => {
   );
 };
 
+function ContractMainPoints() {
+  // TODO: get actual points
+  const points = ["5 years of work guaranteed", "NDA required", "Covers both family dental and health insurance.",
+    "Required relocation to Antarctica", "The soul of your first newborn son required", "Not responsible for death, injury, or illness"];
+  
+  return <div className="ContractMainPoints">
+    <h1>Contract Verdict:</h1>
+    <h1>not good</h1>
+    <div className="Points">
+      <ul>
+        {points.map((curPoint, index) => (
+          <li key={index}>{curPoint}</li> // Use key for each list item
+        ))}
+      </ul>
+    </div>
+  </div>;
+}
+
+function NegotiationPoints() {
+  // TODO: get actual points
+  const values = [
+    ["Required relocation to Antarctica", "You live in Argentina. The commute to Antarctica is only 1.4 hours. Perhaps you could bring up the possibility of commuting?"],
+    ["9% lower salary on average", "UX Writers and Content Designers with 3 years of experience get paid $105k on average."],
+    ["The soul of your firstborn son required", "UX Writers and Content Designers are usually not required to give up the soul of their firstborn son. Maybe you can bring up your awesome Excel skills?"]
+  ];
+
+  return (
+    <div className="NegotiationPointsContainer">
+      <h1>Negotiation Points:</h1>
+      <div className="NegotiationPoints">
+      {values.flat().map((item, index) => (
+        <div className="grid-item" key={index}>
+          {item}
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
 function DashboardPage() {
   return (
     <div className="App">
       <NavBar />
       <ContractHighlights />
       <AnalyticsGraphs />
+      <ContractMainPoints />
+      <NegotiationPoints />
       <BottomBanner />
     </div>
   );
