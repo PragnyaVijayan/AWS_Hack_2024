83165bf490bed92fe9b558db048bca404a80bb6c
TiffanyN22 <tnguyen22@ndsj.org>
2024-11-16 16:56:14 -0800

    Added side pencil

--- ascend/src/App.js
+++ ascend/src/App.js
@@ -4,10 +4,11 @@ import logo from './Assets/Logo.png';
 import paperImg from './Assets/Home/paper graphics.png';
 import pencilImg from './Assets/Home/sharp ver.png';
 import capyImg from './Assets/Home/capy.png';
+import sidePencilImg from './Assets/Home/logo stretch.png'
 
 function NavBar() {
   return <div className="Bar">
-    <img className="NavbarLogo" src={logo} />
+    <img className="NavbarLogo" src={logo} alt="logo" />
     <a className="NavbarItem">Dashboard</a>
     <a className="NavbarItem">New</a>
     <a className="NavbarItem">About</a>
@@ -16,8 +17,8 @@ function NavBar() {
 
 function Header() {
   return <div className="Header">
-    <img className="PencilImg" src={pencilImg} />
-    <img className="PaperImg" src={paperImg} />
+    <img className="PencilImg" src={pencilImg} alt=""/>
+    <img className="PaperImg" src={paperImg} alt =""/>
     <div className="HeaderText">
       <h1>Ascend</h1>
       <h2>fly high.</h2>
@@ -84,13 +85,18 @@ function Creators(){
 function CreatorCard({ image, name, role }) {
   return (
     <div className="CreatorCard">
-      <img src={image} />
+      <img src={image} alt=""/>
       <h1>{name}</h1>
       <h2>{role}</h2>
     </div>
   );
 }
 
+function SidePencil() {
+  return (
+    <img className="SidePencil" src={sidePencilImg} alt=""/>
+  );
+}
 
 function App() {
   return (
@@ -99,6 +105,7 @@ function App() {
       <Header />
       <WhatsAscend />
       <Creators />
+      <SidePencil />
     </div>
   );
 }
